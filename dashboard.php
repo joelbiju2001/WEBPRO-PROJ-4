a93bfdca0d617b1c4b523ba549dfaa69a7f16a25
oscar-g19 <oscarg19@me.com>
2023-05-02 19:49:41 -0400

    dashboard

---
+++ dashboard.php
@@ -0,0 +1,21 @@
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Homepage</title>
+    <link rel="stylesheet" type="text/css" href="dashboard.css">
+  </head>
+  <body>
+    <?php
+session_start();
+// User is logged in, show the dashboard
+echo "Welcome, " . $_SESSION['username'] . "!";
+
+?>
+    
+    <p>Please choose one of the following options:</p>
+    <button onclick="location.href='withdraw.html'">Withdraw</button>
+    <button onclick="location.href='deposit.html'">Deposit</button>
+  <button onclick="location.href='home.html'">Log Out</button>
+  </body>
+</html>
